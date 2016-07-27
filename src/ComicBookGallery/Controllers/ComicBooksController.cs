d05efbd2990d83ecca436b3755228b4155867e0d
amhughes617 <amhughes@g.cofc.edu>
2016-07-27 09:36:56 -0700

    added properties to comicbook

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,30 +10,10 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        private ComicBookRepository _comicBookRepository = null;
-
-        public ComicBooksController()
+        public ActionResult Detail()
         {
-            _comicBookRepository = new ComicBookRepository();
-        }
 
-        public ActionResult Index()
-        {
-            var comicBooks = _comicBookRepository.GetComicBooks();
-
-            return View(comicBooks);
-        }
-
-        public ActionResult Detail(int? id)
-        {
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
-
-            var comicBook = _comicBookRepository.GetComicBook((int)id);
-
-            return View(comicBook);
+            return View();
         }
     }
 }
